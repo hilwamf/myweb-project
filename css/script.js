176641346b52a9a571aa3c0599e269afde022947
Hilwatul Maghfiroh <hilwatulmaghfiroh@gmail.com>
2026-03-01 20:59:18 +0700

    resolving js

---
+++ css/script.js
@@ -0,0 +1,72 @@
+const registerForm =
+document.getElementById("registerForm");
+
+if(registerForm)
+{
+
+registerForm.addEventListener("submit", function(e)
+{
+
+e.preventDefault();
+
+let name =
+document.getElementById("name").value;
+
+let email =
+document.getElementById("email").value;
+
+let institution =
+document.getElementById("institution").value;
+
+let subject =
+document.getElementById("subject").value;
+
+
+/* NAME VALIDATION */
+let namePattern =
+/^[a-zA-Z\s']+$/;
+
+/* EMAIL VALIDATION */
+let emailPattern =
+/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if(!namePattern.test(name))
+{
+alert(
+"Name can only contain letters and apostrophes"
+);
+return;
+}
+
+if(!emailPattern.test(email))
+{
+alert(
+"Invalid email format"
+);
+return;
+}
+
+
+if(institution.length < 3)
+{
+alert(
+"Institution must be at least 3 characters"
+);
+return;
+}
+
+
+if(subject.length < 3)
+{
+alert(
+"Subject must be at least 3 characters"
+);
+return;
+}
+
+
+alert("Application submitted successfully 🎉");
+
+});
+}
+
